7e684967ae5a23fb71245d30339610a0e7910124
Akshansh246 <akshanshsaraf40@gmail.com>
2025-06-07 23:17:14 +0530

    Question 17

---
+++ Q17.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Q17 {
+    public static void findIntersection(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                // Avoid adding duplicates
+                if (result.isEmpty() || result.get(result.size() - 1) != arr1[i]) {
+                    result.add(arr1[i]);
+                }
+                i++;
+                j++;
+            } else if (arr1[i] < arr2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        System.out.println("Intersection: " + result);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 2, 3, 4};
+        int[] arr2 = {2, 2, 4, 6};
+        findIntersection(arr1, arr2);
+    }
+}
