10b7fcf98ba808efa6d79c1f547106328bcf3814
Akshansh246 <akshanshsaraf40@gmail.com>
2025-06-10 23:10:35 +0530

    Question 20

---
+++ Q20.java
@@ -0,0 +1,17 @@
+public class Q20 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+        int maxProduct = nums[0];
+        int currMax = nums[0];
+        int currMin = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int temp = currMax;
+            currMax = Math.max(nums[i], Math.max(currMax * nums[i], currMin * nums[i]));
+            currMin = Math.min(nums[i], Math.min(temp * nums[i], currMin * nums[i]));
+            maxProduct = Math.max(maxProduct, currMax);
+        }
+        System.out.println("Maximum Product Subarray: " + maxProduct);
+    }
+    
+}
